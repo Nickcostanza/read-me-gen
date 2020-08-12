5e977d04b841e05629930ead98e8ba7fc16b34cd
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-08-12 13:59:49 -0700

    array of questions done, along with package.json file created

---
+++ index.js
@@ -0,0 +1,66 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const CheckboxPrompt = require('inquirer/lib/prompts/checkbox');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+//function with array of questions to prompt user
+
+function userInput() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your Project?',
+            name: 'title' 
+        },
+        {
+            type: 'input',
+            message: 'Enter description of your Project?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions?',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'What is your application used for?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Who contributed on this project?',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'What are the test instructions?',
+            name: 'test'
+        },
+        {
+            type: 'checkbox',
+            message: 'Please select a License',
+            choices: [
+                'MIT',
+                'Apache',
+                'ISC',
+            ],
+            name: 'license'
+         },
+         {
+             type: 'input',
+             message: 'What is your GitHub Username?',
+             name: 'username'
+         },
+         {
+             type: 'input',
+             message: 'What is your Email Address?',
+             name: 'email'
+         },
+
+         
+    ]);
+}
